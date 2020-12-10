1b6857926be5e8047b83191fe1e9e56a41b410ec
Coca-hjy <472458040@qq.com>
2020-12-10 14:05:59 +0800

    up

--- python基础/认识python.py
+++ python基础/认识python.py
@@ -232,6 +232,10 @@ import random
       鼠牛虎兔龙蛇马羊猴鸡狗猪
       
       chinese_zodiac="猴鸡狗猪鼠牛虎兔龙蛇马羊"
-      str_year=int()
+      str_year=input("请输入您的出生年份:")
+      int_year=int(str_year)
+      zodiac_num=int_year%12
+
+      print("您的生肖是%s"%chinese_zodiac[zodiac_num])
 '''
 
