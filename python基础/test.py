fb40e2a36b3987a017b63b081cf23bedc629722a
Coca-hjy <472458040@qq.com>
2020-12-11 10:19:18 +0800

    typle

--- python基础/test.py
+++ python基础/test.py
@@ -102,6 +102,53 @@ print(zodiac[n])
     无限次接收输入用户的年月日，判断用户的生肖和星座，并且统计出各个生肖和星座的人数
 '''
 
+'''
+chinese_zodiacs="猴鸡狗猪鼠牛虎兔龙蛇马羊"
+zodiacs=("摩羯座","水瓶座","双鱼座","白羊座","金牛座","双子座","巨蟹座","狮子座","处女座","天秤座","天蝎座","射手座")
+zodiac_days=((1,20),(2,19),(3,21),(4,20),(5,21),(6,22),(7,23),(8,23),(9,23),(10,24),(11,23),(12,22))
+
+# chinese_zodiacs_dict={}
+# for chinese_zodiac in chinese_zodiacs:
+#     chinese_zodiacs_dict[chinese_zodiac]=0
+# print(chinese_zodiacs_dict)
+
+
+
+# {key:value:for key in keys}
+#字典的推导式
+
+chinese_zodiacs_dict={chinese_zodiac:0 for chinese_zodiac in chinese_zodiacs}
+zodiac_dict={zodiac:0 for zodiac in zodiacs}
+# print(chinese_zodiacs_dict)
+# print(zodiac_dict)
+
+while True:
+    year=int(input("请您输入你的呢出生年份："))
+    month=int(input("请输入您的出生月份:"))
+    day=int(input("请输入您的出生日期:"))
+
+    num=0
+    for zodiac_day in zodiac_days:
+        if (month,day) > zodiac_day:
+            num += 1
+        elif month == 12 and day > 22:
+            num = 0
+    chinese_zodiacs_dict[chinese_zodiacs[year%12]] += 1
+    zodiac_dict[zodiacs[num]] += 1
+
+    print(chinese_zodiacs_dict)
+    print(zodiac_dict)
+'''
+# zodiac_dict={}
+
+
+
+
+
+
+
+
+
 #第三章作业集锦
 # a="hello world"
 # print(a.count("l"))
@@ -113,12 +160,14 @@ print(zodiac[n])
 
 # print("最终您的家庭地址是:%s" %sheng+shi+xian)
 
+
+'''
 print("欢迎来到我的世界！")
 username=input("请输入您的账号:")
 password=input("请输入您的密码:")
 if username == "472458040" and password == "123":
     print("登录成功！")
-    choice=["1.添加名片","2.删除名片","3.修改名片","4.查询名片","5.退出系统","6.退出"]
+    choice=["1.添加名片","2.删除名片","3.修改名片","4.查询名片","5.退出系统"]
     print(choice)
     input("请输入您要选择的序列号:")
         # if choice == 0:
@@ -132,6 +181,21 @@ else:
     username=input("请输入您的账号:")
     password=input("请输入您的密码:")
     
+'''
+
+while True:
+    print("*"*30)
+    
+    print("1.添加名片")
+    print("2.删除名片")
+    print("3.修改名片")
+    print("4.查询名片")
+    print("5.退出系统")
+    a=int(input("请输入您需要的序列号:"))
+    
+    if  a == 5:
+        print("退出成功!")
+        break
 
 
 
